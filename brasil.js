8aefa5dc5e7000106a829186b1c60d572e1d5284
Renato Gama <renatoargh@gmail.com>
2013-07-08 14:16:46 -0300

    retirei dependencis do ie. implementei tabela de estados do ibge e metodo de busca

--- brasil.js
+++ brasil.js
@@ -1,6 +1,54 @@
-var ie = require("inscricaoestadual"); 
+module.exports.obterEstado = function(id){
+	if(typeof id === "number"){
+		for(var estado in tabelaIbgeDeEstados){
+			if(tabelaIbgeDeEstados.hasOwnProperty(estado)){
+				estado = tabelaIbgeDeEstados[estado];
+				if(estado.codigo === id)
+					return estado;
+			};
+		}
+	}
+	if(typeof id === "string"){
+		for(var estado in tabelaIbgeDeEstados){
+			if(tabelaIbgeDeEstados.hasOwnProperty(estado)){
+				estado = tabelaIbgeDeEstados[estado];
+				if(estado.nome === id || estado.abreviacao === id)
+					return estado;
+			};
+		}
+	}
+};
 
-module.exports.inscricaoEstadual = ie;
+module.exports.tabelaIbgeDeEstados = tabelaIbgeDeEstados;
+var tabelaIbgeDeEstados = [
+	{ codigo: 11, regiao: "Região Norte", nome: "Rondônia", abreviacao: "RO" },
+	{ codigo: 12, regiao: "Região Norte", nome: "Acre", abreviacao: "AC" },
+	{ codigo: 13, regiao: "Região Norte", nome: "Amazonas", abreviacao: "AM" },
+	{ codigo: 14, regiao: "Região Norte", nome: "Roraima", abreviacao: "RR" },
+	{ codigo: 15, regiao: "Região Norte", nome: "Pará", abreviacao: "PA" },
+	{ codigo: 16, regiao: "Região Norte", nome: "Amapá", abreviacao: "AP" },
+	{ codigo: 17, regiao: "Região Norte", nome: "Tocantins", abreviacao: "TO" },
+	{ codigo: 21, regiao: "Região Nordeste", nome: "Maranhão", abreviacao: "MA" },
+	{ codigo: 22, regiao: "Região Nordeste", nome: "Piauí", abreviacao: "PI" },
+	{ codigo: 23, regiao: "Região Nordeste", nome: "Ceará", abreviacao: "CE" },
+	{ codigo: 24, regiao: "Região Nordeste", nome: "Rio Grande do Norte", abreviacao: "RN" },
+	{ codigo: 25, regiao: "Região Nordeste", nome: "Paraíba", abreviacao: "PB" },
+	{ codigo: 26, regiao: "Região Nordeste", nome: "Pernambuco", abreviacao: "PE" },
+	{ codigo: 27, regiao: "Região Nordeste", nome: "Alagoas", abreviacao: "AL" },
+	{ codigo: 28, regiao: "Região Nordeste", nome: "Sergipe", abreviacao: "SE" },
+	{ codigo: 29, regiao: "Região Nordeste", nome: "Bahia", abreviacao: "BA" },
+	{ codigo: 31, regiao: "Região Sudeste", nome: "Minas Gerais", abreviacao: "MG" },
+	{ codigo: 32, regiao: "Região Sudeste", nome: "Espírito Santo", abreviacao: "ES" },
+	{ codigo: 33, regiao: "Região Sudeste", nome: "Rio de Janeiro", abreviacao: "RJ" },
+	{ codigo: 35, regiao: "Região Sudeste", nome: "São Paulo", abreviacao: "SP" },
+	{ codigo: 41, regiao: "Região Sul", nome: "Paraná", abreviacao: "PR" },
+	{ codigo: 42, regiao: "Região Sul", nome: "Santa Catarina", abreviacao: "SC" },
+	{ codigo: 43, regiao: "Região Sul", nome: "Rio Grande do Sul", abreviacao: "RS" },
+	{ codigo: 50, regiao: "Região Centro-Oeste", nome: "Mato Grosso do Sul", abreviacao: "MS" },
+	{ codigo: 51, regiao: "Região Centro-Oeste", nome: "Mato Grosso", abreviacao: "MT" },
+	{ codigo: 52, regiao: "Região Centro-Oeste", nome: "Goiás", abreviacao: "GO" },
+	{ codigo: 53, regiao: "Região Centro-Oeste", nome: "Distrito Federal", abreviacao: "DF" }
+];
 
 module.exports.formatarChaveDeAcesso = formatarChaveDeAcesso; 
 function formatarChaveDeAcesso(chave){
--- testes.js
+++ testes.js
@@ -1,16 +1,25 @@
-var brasil = require("./brasil");
+var br = require("./brasil");
 
-console.log(brasil.registroNacional("11.222.333/0001-81"));
-console.log(brasil.registroNacional("00.132.781/0001-78"));
-console.log(brasil.registroNacional("016.591.491-26"));
+console.log(br.registroNacional("11.222.333/0001-81"));
+console.log(br.registroNacional("00.132.781/0001-78"));
+console.log(br.registroNacional("016.591.491-26"));
 
-console.log(brasil.cnpj("11.222.333/0001-81"));
-console.log(brasil.cnpj("00.132.781/0001-78"));
+console.log(br.cnpj("11.222.333/0001-81"));
+console.log(br.cnpj("00.132.781/0001-78"));
 
-console.log(brasil.cpf("016.591.491-26"));
+console.log(br.cpf("016.591.491-26"));
 
-console.log(brasil.chaveDeAcesso("52060433009911002506550120000007800267301615"));
-console.log(brasil.chaveDeAcesso("52110210132781000178550010000005480000005486"));
+console.log(br.chaveDeAcesso("52060433009911002506550120000007800267301615"));
+console.log(br.chaveDeAcesso("52110210132781000178550010000005480000005486"));
 
-console.log(brasil.formatarChaveDeAcesso("52110210132781000178550010000005480000005486"));
-console.log(brasil.formatarChaveDeAcesso("52060433009911002506550120000007800267301615"))
+console.log(br.formatarChaveDeAcesso("52110210132781000178550010000005480000005486"));
+console.log(br.formatarChaveDeAcesso("52060433009911002506550120000007800267301615"));
+
+console.log(br.obterEstado(52));
+console.log(br.obterEstado("DF"));
+console.log(br.obterEstado("GO"));
+console.log(br.obterEstado("MG"));
+console.log(br.obterEstado("RJ"));
+console.log(br.obterEstado("Rio de Janeiro"));
+console.log(br.obterEstado("Goiás"));
+console.log(br.obterEstado("Minas Gerais"));
